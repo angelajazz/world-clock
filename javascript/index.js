78311dbbf62af5e3e5d35739849cf814ef9dbe97
angelajazz <ajggutierrez28@gmail.com>
2022-11-18 16:49:32 +0800

    included return link and updated style

--- javascript/index.js
+++ javascript/index.js
@@ -34,14 +34,13 @@ function changeCity(event) {
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
   <div class="city">
-    <div>
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
-    </div>
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
     "A"
   )}</small></div>
   </div>
+  <a href="index.html">Back to Main</a>
   `;
 }
 
